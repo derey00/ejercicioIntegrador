3030929fd3ab0672fffb20e778e1e112d3efdd5c
derey00 <71334124+derey00@users.noreply.github.com>
2024-09-20 11:57:34 +0200

    Add files via upload

---
+++ src/main/java/elena/ejercicioIntegrador/EjercicioIntegradorApplication.java
@@ -0,0 +1,13 @@
+package elena.ejercicioIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioIntegradorApplication.class, args);
+	}
+
+}
